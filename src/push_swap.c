48b6f867f4b89489600e0bfd9ab007132e15cf65
shoudek <stepan.houdek1@gmail.com>
2024-02-13 10:24:26 +0100

    Update: rr, rra_rrb, rrr, sa_sb

--- src/push_swap.c
+++ src/push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shoudek <shoudek@student.42.cz>            +#+  +:+       +#+        */
+/*   By: shoudek <shoudek@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 14:06:44 by shoudek           #+#    #+#             */
-/*   Updated: 2024/02/09 14:23:58 by shoudek          ###   ########.fr       */
+/*   Updated: 2024/02/13 10:23:50 by shoudek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ void	pa_pb(t_struct **head_a, t_struct **head_b, t_struct **tail_b)
 		return ;
 	temp = (*head_a)->prev;
 	// if there's nothing on stack b
-	if ((*head_b) == NULL) 
+	if ((*head_b) == NULL)
 	{
-	// push the node from A to start of B
+		// push the node from A to start of B
 		*head_b = *head_a;
 		*tail_b = *head_a;
 		(*head_b)->prev = NULL;
@@ -56,6 +56,69 @@ void	ra_rb(t_struct **tail, t_struct **head)
 	return ;
 }
 
+void	rr(t_struct **t_a, t_struct **h_a, t_struct **t_b, t_struct **h_b)
+{
+	ra_rb(t_a, h_a);
+	ra_rb(t_b, h_b);
+}
+
+// swap the first 2 elements at the top of stack
+// do nothing if ther's only one or no elements
+void	sa_sb(t_struct **tail, t_struct **head)
+{
+	t_struct	*temp_head;
+
+	if (*tail == *head)
+		return ;
+	if ((*tail)->next == *head)
+	{
+		temp_head = *head;
+		*head = *tail;
+		*tail = temp_head;
+		(*tail)->next = *head;
+		(*tail)->prev = NULL;
+		(*head)->prev = *tail;
+		(*head)->next = NULL;
+		return ;
+	}
+	temp_head = *head;
+	*head = (*head)->prev;
+	temp_head->prev = (*head)->prev;
+	temp_head->next = *head;
+	temp_head->prev->next = temp_head;
+	(*head)->prev = (*head)->next;
+	(*head)->next = NULL;
+	return ;
+}
+
+void	rra_rrb(t_struct **tail, t_struct **head)
+{
+	t_struct	*temp_tail;
+
+	temp_tail = *tail;
+	(*head) = (*head)->prev;
+	*tail = (*head)->next;
+	(*head)->next = NULL;
+	(*tail)->prev = NULL;
+	(*tail)->next = temp_tail;
+	temp_tail->prev = (*tail);
+	return ;
+}
+
+void	rrr(t_struct **t_a, t_struct **h_a, t_struct **t_b, t_struct **h_b)
+{
+	rra_rrb(t_a, h_a);
+	rra_rrb(t_b, h_b);
+	return ;
+}
+
+void	calc_price(t_struct *h_a, t_struct *t_a, t_struct *h_b, t_struct *t_b)
+{
+	if (h_a->x > h_b->x)
+		h_a->price = 1;
+	return ;
+}
+
 void	push_swap(void)
 {
 	t_struct	*tail_a;
@@ -70,48 +133,51 @@ void	push_swap(void)
 	ft_dublstinit(&tail_a, &head_a, 3);
 	// push numbers to stack A
 	ft_dublstadd_start(&tail_a, 8);
-	ft_dublstadd_start(&tail_a, 4); 
+	ft_dublstadd_start(&tail_a, 4);
 	ft_dublstadd_start(&tail_a, 9);
+	//print stack_a from head to tail
+	printf("Stack A original:\n");
+	curr = head_a;
+	while (curr != NULL)
+	{
+		printf("%d\n", curr->x);
+		curr = curr->prev;
+	}
+	sa_sb(&tail_a, &head_a);
 	// print stack_a from head to tail
-	printf("Stack A:\n");
+	printf("Stack A sa_sb:\n");
 	curr = head_a;
 	while (curr != NULL)
 	{
 		printf("%d\n", curr->x);
 		curr = curr->prev;
 	}
-	// rotate stack A
+	rra_rrb(&tail_a, &head_a);
+	// print stack_a from head to tail
+	printf("Stack A rra_rrb:\n");
+	curr = head_a;
+	while (curr != NULL)
+	{
+		printf("%d\n", curr->x);
+		curr = curr->prev;
+	}
 	ra_rb(&tail_a, &head_a);
-	// print stack_a
-	printf("Stack A:\n");
+	// print stack_a from head to tail
+	printf("Stack A ra_rb:\n");
 	curr = head_a;
 	while (curr != NULL)
 	{
 		printf("%d\n", curr->x);
 		curr = curr->prev;
 	}
-	// push a number to stack_b
+	// push two numbers to stack_b
 	pa_pb(&head_a, &head_b, &tail_b);
-	// print stack_b
-	printf("Stack B:\n");
-	curr = head_b;
-	while (curr != NULL)
-	{
-		printf("%d\n", curr->x);
-		curr = curr->prev;
-	}
-	// print stack_a
-	printf("Stack A:\n");
-	curr = head_a;
-	while (curr != NULL)
-	{
-		printf("%d\n", curr->x);
-		curr = curr->prev;
-	}
-	// push a number to stack_b
 	pa_pb(&head_a, &head_b, &tail_b);
+	// if tail is bigger than head, reverse
+	if (head_b->x < tail_b->x)
+		ra_rb(&tail_b, &head_b);
 	// print stack_b
-	printf("Stack B:\n");
+	printf("Stack B pa_pb dvakrat:\n");
 	curr = head_b;
 	while (curr != NULL)
 	{
