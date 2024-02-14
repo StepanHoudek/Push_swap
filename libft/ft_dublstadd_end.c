df343147de78daa445216c4411d15f5ca1c9c007
StepanHoudek <stepan.houdek1@gmail.com>
2024-02-14 13:24:25 +0100

    Update: init r and rr price

--- libft/ft_dublstadd_end.c
+++ libft/ft_dublstadd_end.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_dublistadd_end.c                                :+:      :+:    :+:   */
+/*   ft_dublstadd_end.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: shoudek <shoudek@student.42.cz>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 16:40:46 by shoudek           #+#    #+#             */
-/*   Updated: 2024/02/08 16:43:16 by shoudek          ###   ########.fr       */
+/*   Updated: 2024/02/14 13:24:14 by shoudek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	ft_dublstadd_end(t_struct **head, int value)
 	if (new_node == NULL)
 		return ;
 	new_node->x = value;
+	new_node->r_price = 0;
+	new_node->rr_price = 0;
 	new_node->prev = *head;
 	new_node->next = NULL;
 	(*head)->next = new_node;
